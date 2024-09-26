1acd1131bf5d328cec68a3dc66f62b56b97a56c2
Daniel M Hooper <102091502+dhooper1@users.noreply.github.com>
2024-09-26 14:29:20 +0200

    Update filter_molecules.sh

--- Scripts/filter_molecules.sh
+++ Scripts/filter_molecules.sh
@@ -3,6 +3,6 @@
 ## This script removes molecules with ambiguous BX codes, Phred mapping QUAL <50, and with less than four associated read pairs
 
 input=$1
-output=`echo $input | cut -f1-4 -d"."`
+output=`echo $input | cut -f1-3 -d"."`
 
 awk '!/A00/ && !/B00/ && !/D00/ && $5 >= 50 && $10 >= 4' $input > $output.filter.bed
